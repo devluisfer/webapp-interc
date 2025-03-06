2e7f0e92bd366b02465844a84d8b88d83d85b2ee
Luis Ojeda <lojedap@medialabla.com>
2025-03-05 20:28:25 -0500

    styles flex col animate spin

--- src/app/products/[sku]/loading.tsx
+++ src/app/products/[sku]/loading.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css';
 export default function Loading() {
     return (
-      <div className="text-center mt-10">
+      <div className="text-center mt-10 flex flex-col items-center">
         <div className="animate-spin rounded-full h-24 w-24 border-8 border-gray-900 border-t-transparent"></div>
         <p className="text-gray-600 text-lg">Cargando producto...</p>
       </div>
